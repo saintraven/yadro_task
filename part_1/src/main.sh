a321dfcd76477e40eecfa845f05e25bcf9463c54
saintraven <vitalmoroz42@gmail.com>
2026-05-11 17:46:10 +0300

    write Dockerfile and fix for log path

--- part_1/src/main.sh
+++ part_1/src/main.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
 url="https://lichess.org"
+LOG_FILE="/app/log/log.txt"
 
 for i in {1..5}; do
   response=$(curl -s -w "%{http_code}" "$url")
   http_code="${response: -3}"          
   body="${response%???}"        
-    
-    if [ "$http_code" -ge 100 ] && [ "$http_code" -lt 400 ]; then
-        echo "Number of request: $i" >> ../log/log.txt
-        echo "$http_code" >> ../log/log.txt
-        echo "$body" >> ../log/log.txt
+
+  if [ "$http_code" -ge 100 ] && [ "$http_code" -lt 400 ]; then
+        echo "Number of request: $i" >> "$LOG_FILE" 
+        echo "$http_code" >> "$LOG_FILE" 
+        echo "$body" >> "$LOG_FILE" 
     else
         echo "Error $http_code" 
     fi
